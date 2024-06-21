765bb7ca15ebc4ddff70d4fde3e82c3eaab28dea
Ronny John <me@ronnyjohnti.dev>
2024-06-21 02:03:36 -0300

    refactor: Refactoring script to copy wasm_exec.js from GOROOT

--- start.sh
+++ start.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
+
 go mod tidy
 GOOS=js GOARCH=wasm go build -o web/main.wasm
 
-# Inicializa servidor web na porta 5000
+# Copy the Go WebAssembly initializing script to application
+cp $(go env GOROOT)/misc/wasm/wasm_exec.js web/
+
+# Starts a web server on port 5000
 python3 -m http.server 5000 -d web/
 # php -S 0.0.0.0:5000 -t web/
